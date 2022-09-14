5faa9335f0f119e21b0898bab6a040e1934abade
Emerson Souza <em3rals@gmail.com>
2022-09-14 12:19:02 -0300

    Adicionado funções para maniuplaçao de alto nivel em diretorios e criar arquivos compactados

--- txt.py
+++ txt.py
@@ -9,7 +9,7 @@ tell() -> Retorna posição atual do arquivo em bytes
 #programa que lê e escreve e envia o arquivo via e-mail
 #Criar um find no texto
 #Fazer gerar dentro de uma pasta
-import os
+import os, shutil
 
 def menu():
     print("Opcao '1': Inserir um valor.")
@@ -160,5 +160,55 @@ def retorna_list(nomeArquivo):
     return list_arquivo
 
 
+def copiar_conteudo_arquivos():
+    origem = 'src.txt'
+    destino = 'dst.txt'
 
-        
+    #abre um em modo de leitura e o que vai modificar em modo de escrita
+    arquivo1 = open(origem, 'r')
+    arquivo2 = open(destino, 'w')
+
+    shutil.copyfileobj(arquivo1, arquivo2)
+
+def criarCopiaArquivo():
+    #Personalizar nome do arquivo
+    origem = './origem/src'
+    destino = './backup/src(copia2).txt'
+    #shutil.copyfile OU shutil.copy
+    dest = shutil.copyfile(origem, destino)
+    print("Cópia criado no caminho", dest)
+
+def copiarArvoreDiretorios():
+    #PERSONALIZAR NOME DA PASTA
+    origem = r'G:\Meu Drive\PY\Arquivos\backup'
+    destino = 'G:\Meu Drive\PY\Arquivos\copia'
+
+    shutil.copytree(origem, destino)
+
+def removeArvoreDiretorios():
+    path = 'G:\Meu Drive\PY\Arquivos\copia'
+    shutil.rmtree(path)
+
+#Cria o diretoório se não já existir, e move o diretório caso já exista
+#copytree(source, destination, ignore=ignore_patterns('*.pyc', 'tmp*')): copia tudo menos arquivos *.pyc e 
+#começam com tmp. * é o coringa
+def moverDiretorio():
+    origem = r'G:\Meu Drive\PY\Arquivos\backup'
+    destino =     destino = 'G:\Meu Drive\PY\Arquivos\movidosCriado'
+    shutil.move(origem, destino)
+
+#espaço total, usado e livre
+def visualizarEspaço(path = 'G:\Meu Drive'):
+    #implantar pra GB tb
+    uso_disco = shutil.disk_usage(path)
+    mb = uso_disco.used / 1024**2
+    print(f'Você já usou: {round(mb, 2)} MB')
+
+def ziparDiretorio(nomePastaZipada = "backupZip"):
+    nomeMeuZip = f'G:\Meu Drive\PY\Arquivos\{nomePastaZipada}'
+    diretorioBase =r'G:\Meu Drive\PY\Arquivos\backup'
+
+    shutil.make_archive(nomeMeuZip, "zip" ,diretorioBase)
+
+def descompactarZip():
+    shutil.unpack_archive(r'G:\Meu Drive\PY\Arquivos\backupZip.zip', 'G:\Meu Drive\PY\Arquivos', 'zip')
